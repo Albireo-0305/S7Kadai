9a9bdc8775ee01a3bf98ed73fae48afe17adc44c
Albireo <l2404825418as@gmail.com>
2025-04-30 19:33:02 +0900

    S7kadai-1

--- src/main/java/com/example/S7/S7Application.java
+++ src/main/java/com/example/S7/S7Application.java
@@ -6,7 +6,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
